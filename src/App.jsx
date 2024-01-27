6bd0a994d9262b7cf73f3730e6a2e8edec4fff88
Vikash Verma <130314349+Vikashverma2@users.noreply.github.com>
2024-01-27 21:30:07 +0530

    l

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Calculator from './Calculator'
+import './App.css'
+
+
+function App() {
+  return (
+   <>
+   <div className="mainbar">
+   <Calculator/>
+   </div>  
+   </>
+  )
+}
+
+export default App
