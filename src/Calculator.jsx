84125f3565a5aa4baf2ffaa5a8a510047876c149
Vikash Verma <130314349+Vikashverma2@users.noreply.github.com>
2024-01-28 00:43:29 +0530

    responsive update

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -22,25 +22,37 @@ const Calculator = () => {
         <div className="mainBox">
             <div className="calc">
                 <div className="CAL2">
+                   <div className="diplayBox"> 
                     <input type="text" placeholder='0' className='display' value={result} />
-                    <input type="button" value="%" className='btn bt' onClick={clickHandler} />
+                    </div>
+                   <div>
+                   <input type="button" value="%" className='btn bt' onClick={clickHandler} />
                     <input type="button" value="/" className='btn bt' onClick={clickHandler} />
                     <input type="button" value="C" className='btn bt1' onClick={clrDisplay} />
+                   </div>
+                    <div>
                     <input type="button" value="7" className='btn' onClick={clickHandler} />
                     <input type="button" value="8" className='btn' onClick={clickHandler} />
                     <input type="button" value="9" className='btn' onClick={clickHandler} />
                     <input type="button" value="*" className='btn bt' onClick={clickHandler} />
+                    </div>
+                    <div>
                     <input type="button" value="4" className='btn' onClick={clickHandler} />
                     <input type="button" value="5" className='btn' onClick={clickHandler} />
                     <input type="button" value="6" className='btn' onClick={clickHandler} />
                     <input type="button" value="-" className='btn bt' onClick={clickHandler} />
+                    </div>
+                    <div>
                     <input type="button" value="1" className='btn' onClick={clickHandler} />
                     <input type="button" value="2" className='btn' onClick={clickHandler} />
                     <input type="button" value="3" className='btn' onClick={clickHandler} />
                     <input type="button" value="+" className='btn bt' onClick={clickHandler} />
+                    </div>
+                    <div>
                     <input type="button" value="0" className='btn' onClick={clickHandler} />
                     <input type="button" value="." className='btn' onClick={clickHandler} />
                     <input type="button" value="=" className='btn bt2' onClick={calculate} />
+                    </div>
                     <p>Made by - Vikash Verma❤️</p>
                 </div>
             </div>
