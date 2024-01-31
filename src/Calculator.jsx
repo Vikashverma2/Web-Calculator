c40d1e181ec3f9f347c9a741b3685e98697abd45
Vikash Verma <130314349+Vikashverma2@users.noreply.github.com>
2024-01-31 07:33:25 +0530

    Mobile Responsive

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { IoMdBackspace } from "react-icons/io";
 import React from 'react'
 
 const Calculator = () => {
@@ -16,39 +17,43 @@ const Calculator = () => {
     const calculate = () => {
         setResult(eval(result).toString());
     }
+    const back = () => {
+        setResult(result.slice(0, -1));
+    }
 
 
     return (
         <div className="mainBox">
-            <div className="calc">
-                <div className="CAL2">
+            <div className="container">
+                <div className="Card">
                    <div className="diplayBox"> 
                     <input type="text" placeholder='0' className='display' value={result} />
                     </div>
-                   <div>
+                   <div className="Row">
                    <input type="button" value="%" className='btn bt' onClick={clickHandler} />
                     <input type="button" value="/" className='btn bt' onClick={clickHandler} />
+                    <div className="btn  back" onClick={back}> <IoMdBackspace />  </div>
                     <input type="button" value="C" className='btn bt1' onClick={clrDisplay} />
                    </div>
-                    <div>
+                    <div className="Row">
                     <input type="button" value="7" className='btn' onClick={clickHandler} />
                     <input type="button" value="8" className='btn' onClick={clickHandler} />
                     <input type="button" value="9" className='btn' onClick={clickHandler} />
                     <input type="button" value="*" className='btn bt' onClick={clickHandler} />
                     </div>
-                    <div>
+                    <div className="Row">
                     <input type="button" value="4" className='btn' onClick={clickHandler} />
                     <input type="button" value="5" className='btn' onClick={clickHandler} />
                     <input type="button" value="6" className='btn' onClick={clickHandler} />
                     <input type="button" value="-" className='btn bt' onClick={clickHandler} />
                     </div>
-                    <div>
+                    <div className="Row">
                     <input type="button" value="1" className='btn' onClick={clickHandler} />
                     <input type="button" value="2" className='btn' onClick={clickHandler} />
                     <input type="button" value="3" className='btn' onClick={clickHandler} />
                     <input type="button" value="+" className='btn bt' onClick={clickHandler} />
                     </div>
-                    <div>
+                    <div className="Row">
                     <input type="button" value="0" className='btn' onClick={clickHandler} />
                     <input type="button" value="." className='btn' onClick={clickHandler} />
                     <input type="button" value="=" className='btn bt2' onClick={calculate} />
